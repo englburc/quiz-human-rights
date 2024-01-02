37532097166710426e4f6566a25b20851d817bb6
englburc <teacher.levan@gmail.com>
2024-01-02 08:09:51 +0000

    logic.js - added even listeners, startQuiz() function

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,3 +5,21 @@ let timer;
 const startButton = document.getElementById('start');
 const submitButton = document.getElementById('submit');
 const clearButton = document.getElementById('clear');
+
+// Event listeners
+startButton.addEventListener('click', startQuiz);
+submitButton.addEventListener('click', saveScore);
+
+
+
+function startQuiz() {
+    // Initialize variables or reset game state
+    currentQuestionIndex = 0;
+    score = 0;
+
+    // Hide start screen, show questions
+    document.getElementById('start-screen').classList.add('hide');
+    document.getElementById('questions').classList.remove('hide');
+
+}
+
